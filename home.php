64c2d1f413ff84b013b9482e86b2c86d30c66a27
arivianto <ahmad.arivianto@students.amikom.ac.id>
2018-12-15 21:14:02 +0700

    upload

---
+++ home.php
@@ -0,0 +1,81 @@
+    <div class="col-md-3 col-sm-12 mb-2">
+       <div class="card">
+        <img class="card-img-top" src="https://cdn-images-1.medium.com/max/823/1*bqt8t5vPveFWPWc_1FZaRA.jpeg" alt="Card image cap">
+          <div class="card-body">
+            <img class="imgthumb" src="https://bashooka.com/wp-content/uploads/2013/10/flat-logo-designs-14.jpg" height="70" width="70">
+           <h6 class="card-title">Nama</h6>
+           <p class="card-text">Nama Pengguna</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-3 col-sm-12 mb-2">
+       <div class="card">
+        <img class="card-img-top" src="https://cdn-images-1.medium.com/max/823/1*bqt8t5vPveFWPWc_1FZaRA.jpeg" alt="Card image cap">
+          <div class="card-body">
+            <img class="imgthumb" src="https://bashooka.com/wp-content/uploads/2013/10/flat-logo-designs-14.jpg" height="70" width="70">
+           <h6 class="card-title">Nama</h6>
+           <p class="card-text">Nama Pengguna</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-3 col-sm-12 mb-2">
+       <div class="card">
+        <img class="card-img-top" src="https://cdn-images-1.medium.com/max/823/1*bqt8t5vPveFWPWc_1FZaRA.jpeg" alt="Card image cap">
+          <div class="card-body">
+            <img class="imgthumb" src="https://bashooka.com/wp-content/uploads/2013/10/flat-logo-designs-14.jpg" height="70" width="70">
+           <h6 class="card-title">Nama</h6>
+           <p class="card-text">Nama Pengguna</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-3 col-sm-12 mb-2">
+       <div class="card">
+        <img class="card-img-top" src="https://cdn-images-1.medium.com/max/823/1*bqt8t5vPveFWPWc_1FZaRA.jpeg" alt="Card image cap">
+          <div class="card-body">
+            <img class="imgthumb" src="https://bashooka.com/wp-content/uploads/2013/10/flat-logo-designs-14.jpg" height="70" width="70">
+           <h6 class="card-title">Nama</h6>
+           <p class="card-text">Nama Pengguna</p>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-md-3 col-sm-12 mb-2">
+       <div class="card">
+        <img class="card-img-top" src="https://cdn-images-1.medium.com/max/823/1*bqt8t5vPveFWPWc_1FZaRA.jpeg" alt="Card image cap">
+          <div class="card-body">
+            <img class="imgthumb" src="https://bashooka.com/wp-content/uploads/2013/10/flat-logo-designs-14.jpg" height="70" width="70">
+           <h6 class="card-title">Nama</h6>
+           <p class="card-text">Nama Pengguna</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-3 col-sm-12 mb-2">
+       <div class="card">
+        <img class="card-img-top" src="https://cdn-images-1.medium.com/max/823/1*bqt8t5vPveFWPWc_1FZaRA.jpeg" alt="Card image cap">
+          <div class="card-body">
+            <img class="imgthumb" src="https://bashooka.com/wp-content/uploads/2013/10/flat-logo-designs-14.jpg" height="70" width="70">
+           <h6 class="card-title">Nama</h6>
+           <p class="card-text">Nama Pengguna</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-3 col-sm-12 mb-2">
+       <div class="card">
+        <img class="card-img-top" src="https://cdn-images-1.medium.com/max/823/1*bqt8t5vPveFWPWc_1FZaRA.jpeg" alt="Card image cap">
+          <div class="card-body">
+            <img class="imgthumb" src="https://bashooka.com/wp-content/uploads/2013/10/flat-logo-designs-14.jpg" height="70" width="70">
+           <h6 class="card-title">Nama</h6>
+           <p class="card-text">Nama Pengguna</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-3 col-sm-12 mb-2">
+       <div class="card">
+        <img class="card-img-top" src="https://cdn-images-1.medium.com/max/823/1*bqt8t5vPveFWPWc_1FZaRA.jpeg" alt="Card image cap">
+          <div class="card-body">
+            <img class="imgthumb" src="https://bashooka.com/wp-content/uploads/2013/10/flat-logo-designs-14.jpg" height="70" width="70">
+           <h6 class="card-title">Nama</h6>
+           <p class="card-text">Nama Pengguna</p>
+          </div>
+        </div>
+      </div>
