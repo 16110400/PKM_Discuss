403e40c12b2d5433fda0838eb0e7ea1fb11461e9
iluthtaqwim <iluth21.i2@gmal.com>
2018-12-15 18:40:23 +0700

    check

---
+++ home.php
@@ -0,0 +1,40 @@
+<div class="main">
+        <div class="container" style="padding-top:50px;padding-bottom: 50px">
+          <div class="row">
+            <div class="col-md-12">
+              <div class="card col-md-3">
+               <img class="img-thumbnail" src="images/vid.png" alt="Card image cap">
+                <div class="card-body">
+                  <h5><b>Judul</b></h5>
+                  <h6 class="card-subtitle mb-2 text-muted">Akun</h6>
+                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+              </div>
+              <div class="card col-md-3">
+               <img class="img-thumbnail" src="images/vid.png" alt="Card image cap">
+                <div class="card-body">
+                  <h5><b>Judul</b></h5>
+                  <h6 class="card-subtitle mb-2 text-muted">Akun</h6>
+                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+              </div>
+              <div class="card col-md-3">
+               <img class="img-thumbnail" src="images/vid.png" alt="Card image cap">
+                <div class="card-body">
+                  <h5><b>Judul</b></h5>
+                  <h6 class="card-subtitle mb-2 text-muted">Akun</h6>
+                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+              </div>
+              <div class="card col-md-3">
+               <img class="img-thumbnail" src="images/vid.png" alt="Card image cap">
+                <div class="card-body">
+                  <h5><b>Judul</b></h5>
+                  <h6 class="card-subtitle mb-2 text-muted">Akun</h6>
+                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+              </div>
+            </div>    
+          </div>
+        </div>
+    </div>
--- upload.php
+++ upload.php
@@ -1,5 +1,3 @@
-<link rel="stylesheet" href="css/style.css">
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
 
 <div class="container">
     <div>
